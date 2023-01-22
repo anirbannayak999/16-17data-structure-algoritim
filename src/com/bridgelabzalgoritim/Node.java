56fdd4a542b2281a741e536da5ac92ec3673c97f
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-22 20:19:42 +0530

    Palindromechecker

---
+++ src/com/bridgelabzalgoritim/Node.java
@@ -0,0 +1,24 @@
+package com.bridgelabzalgoritim;
+
+public class Node<R> {
+    R data;
+    Node<R> next;
+    public Node(){
+        this.data=data;
+        this.next=null;
+
+    }
+    public Node(R data){
+        this.data=data;
+
+
+
+    }
+    @Override
+    public String toString() {
+        return "[data=" + data + ", next=" + next + "]";
+    }
+
+
+}
+
---
+++ src/com/bridgelabzalgoritim/Plindromechecker.java
@@ -0,0 +1,35 @@
+package com.bridgelabzalgoritim;
+
+public class Plindromechecker {
+    public class Stack<E> {
+        private LinkedList<E> linkedList = new LinkedList<>();
+
+        /* adding items in stack (adding tail) */
+        public void push(E item) {
+            linkedList.append(item);
+        }
+
+        /* showing the full stack */
+        public void printStack() {
+            linkedList.display();
+        }
+
+        /* deleting item in stack (delete tail) */
+        public void pop() {
+            linkedList.popLast();
+        }
+
+        /* Just getting the last value added in stack */
+        Node<E> peak() {
+            return linkedList.getTail();
+        }
+
+        /* searching a value in stack */
+        public boolean searchData(E data) {
+            if (linkedList.search(data) != null)
+                return true;
+            return false;
+        }
+
+    }
+}
